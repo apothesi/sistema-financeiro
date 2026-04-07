8b65bf9d9809a3f69ea76548d46b1bf31fc96996
Murilo Souza Targa <targasouza1@gmail.com>
2026-04-07 08:11:54 -0300

    Add navigation menu for financial system

---
+++ sistema financeiro/includes/menu.php
@@ -0,0 +1,33 @@
+<?php $perfil = $_SESSION["perfil"]; ?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+    <div class="container">
+        <a class="navbar-brand" href="../pages/dashboard.php">Sistema Financeiro</a>
+        <ul class="navbar-nav ms-auto">
+
+            <?php if ($perfil == "cliente") { ?>
+                <!-- falta o link de Minhas Contas -->
+            <?php } ?>
+
+            <?php if ($perfil == "empresa") { ?>
+                <li class="nav-item">
+                    <a href="cadastrar.php">Cadastrar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="CAMINHO">TEXTO</a>
+                </li>
+            <?php } ?>
+
+            <?php if ($perfil == "proprietario") { ?>
+                <li class="nav-item">
+                    <a href="cadastrar.php">Cadastrar</a>
+                </li>
+            <?php } ?>
+
+            <li class="nav-item">
+                <a href="../logout.php">Sair</a>
+            </li>
+
+        </ul>
+    </div>
+</nav>
