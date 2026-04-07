b1e77669e67010eb670c1e8cdf2692c3ee9bb04d
Murilo Souza Targa <targasouza1@gmail.com>
2026-04-07 10:41:40 -0300

    Update menu for user profiles and add links

--- sistema financeiro/includes/menu.php
+++ sistema financeiro/includes/menu.php
@@ -1,33 +1,45 @@
-<?php $perfil = $_SESSION["perfil"]; ?>
+<?php
+$perfil = $_SESSION["perfil"];
+?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-    <div class="container">
-        <a class="navbar-brand" href="../pages/dashboard.php">Sistema Financeiro</a>
-        <ul class="navbar-nav ms-auto">
+  <div class="container">
+    <a class="navbar-brand" href="#">Sistema Financeiro</a>
+    
+    <ul class="navbar-nav ms-auto">
 
-            <?php if ($perfil == "cliente") { ?>
-                <!-- falta o link de Minhas Contas -->
-            <?php } ?>
+      <li class="nav-item">
+        <a class="nav-link" href="dashboard.php">Dashboard</a>
+      </li>
 
-            <?php if ($perfil == "empresa") { ?>
-                <li class="nav-item">
-                    <a href="cadastrar.php">Cadastrar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=""></a>
-                </li>
-            <?php } ?>
+      <?php if ($perfil == "cliente") { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="contas/listar.php">Minhas Contas</a>
+        </li>
+      <?php } ?>
+      
+      <?php if ($perfil == "empresa") { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="contas/listar.php">Contas da Empresa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contas/cadastrar.php">Cadastrar Conta</a>
+        </li>
+      <?php } ?>
 
-            <?php if ($perfil == "proprietario") { ?>
-                <li class="nav-item">
-                    <a href="cadastrar.php">Cadastrar</a>
-                </li>
-            <?php } ?>
+      <?php if ($perfil == "proprietario") { ?>
+        <li class="nav-item">
+          <a class="nav-link" href="contas/listar.php">Todas as Contas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="usuarios/listar.php">Usuários</a>
+        </li>
+      <?php } ?>
 
-            <li class="nav-item">
-                <a href="../logout.php">Sair</a>
-            </li>
-
-        </ul>
-    </div>
+      <li class="nav-item">
+        <a class="nav-link" href="../logout.php">Sair</a>
+      </li>
+      
+    </ul>
+  </div>
 </nav>
